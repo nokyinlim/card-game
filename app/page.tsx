8d7b3d0618ae0955b734f3389fa74a54acb2e44f
Nok Yin Lim <nylim0506@icloud.com>
2025-02-16 16:04:43 +0800

    Update page

--- app/page.tsx
+++ app/page.tsx
@@ -16,16 +16,18 @@ import { DialogClose } from "@radix-ui/react-dialog";
 export default function Home() {
 
   const nextRouter = useNextRouter();
-  const { query } = nextRouter;
-  const textValue = query.source || "localhost:8000";
-
-  const { toast } = useToast();
-  const router = useRouter();
   const [source, setSource] = useState("localhost:8000");
 
   useEffect(() => {
-    setSource(localStorage.getItem("source") || textValue as string);
-  }, [textValue]);
+    if (typeof window !== 'undefined') {
+      const { query } = nextRouter;
+      const textValue = query.source || "localhost:8000";
+      setSource(localStorage.getItem("source") || textValue as string);
+    }
+  }, [nextRouter]);
+
+  const { toast } = useToast();
+  const router = useRouter();
   
 
   const openLink = (url: string) => {
@@ -34,9 +36,17 @@ export default function Home() {
       description: `You will be redirected to "${url}".`
     })
 
-    setTimeout(() => {
-      router.push(url);
-    }, 1000)
+    if (url in ["/tutorial", "/characters"]) {
+      setTimeout(() => {
+        router.push(url);
+      }, 1000)
+    } else {
+      toast({
+        variant: "destructive",
+        title: "Error!",
+        description: `Invalid Redirect URL: "${url}".`
+      })
+    }
     
   }
 
@@ -85,26 +95,8 @@ export default function Home() {
               <SelectValue placeholder="How To Play" />
             </SelectTrigger>
             <SelectContent position="popper">
+              <SelectItem value="/tutorial">Tutorial</SelectItem>
 
-              <Dialog>
-                <DialogTrigger asChild>
-                  <SelectItem value="/tutorial">Tutorial</SelectItem>
-                </DialogTrigger>
-                <DialogContent>
-                  <DialogHeader>
-                    <DialogTitle>Redirecting to Tutorial...</DialogTitle>
-                    <DialogDescription>
-                    <div className="flex flex-col space-y-3">
-                      <Skeleton className="h-[125px] w-[250px] rounded-xl" />
-                      <div className="space-y-2">
-                        <Skeleton className="h-4 w-[250px]" />
-                        <Skeleton className="h-4 w-[200px]" />
-                      </div>
-                    </div>
-                    </DialogDescription>
-                  </DialogHeader>
-                </DialogContent>
-              </Dialog>
               <SelectItem value="/characters">Explore Characters</SelectItem>
             </SelectContent>
           </Select>
